e4e97c5905722d8e88519441517c7747e164d0ac
refond <refon@pobox.com>
2016-05-20 12:34:26 +0200

    Code clean up.

--- main/test/hbGeoService.service.spec.js
+++ main/test/hbGeoService.service.spec.js
@@ -1,40 +1,34 @@
-describe('coordinatesConversions', function () {
 
-  //beforeEach(module('hbUi.geo').value('clientDebugEnabled', true));
-	
-  beforeEach(function() {
-	  //var clientDebugEnabled = true;
-	  module('hbUi.geo')
-  	}
-  );
+describe('coordinatesConversions', function() {
 
-  
-//Defined out reference variable outside
-  var hbGeoService;
+	beforeEach(function() {
+		module('hbUi.geo')
+	});
 
-  // Wrap the parameter in underscores
-  beforeEach( inject( function(_hbGeoService_){
-	  hbGeoService = _hbGeoService_;
-  }));
+	// Defined out reference variable outside
+	var hbGeoService;
 
-  
+	// Wrap the parameter in underscores
+	beforeEach(inject(function(_hbGeoService_) {
+		hbGeoService = _hbGeoService_;
+	}));
 
-		it('point1 should equal point1bis both in swiss federal coordinates', function () {
-			
-        	var point1 = {"x" : 550000.85, "y" : 150002.65};
-        	var latLng1 = hbGeoService.getLongitudeLatitudeCoordinates(point1.x, point1.y);
-        	
-//        	$log.debug("    >>>>>>>>>>>>>>>>>>>>>>>>>>>>    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<    ");
-//        	$log.debug("point1                : " + angular.toJson(point1));
-//        	$log.debug("point1  => latLng1    : " + angular.toJson(latLng1));
-        	var point1bis = hbGeoService.getSwissFederalCoordinates(latLng1.lat, latLng1.lng);
-//        	$log.debug("latLng1 => point1bis  : " + angular.toJson(point1bis));
-//
-//        	$log.debug("    >>>>>>>>>>>>>>>>>>>>>>>>>>>>    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<    ");			
+	it('point1 should equal point1bis both in swiss federal coordinates',
+			function() {
 
-			expect(point1bis.lat).toBe(point1.lat);
-			expect(point1bis.lng).toBe(point1.lng);
-		});	
+				var point1 = {
+					"x" : 550000.85,
+					"y" : 150002.65
+				};
+				var latLng1 = hbGeoService.getLongitudeLatitudeCoordinates(
+						point1.x, point1.y);
+				var point1bis = hbGeoService.getSwissFederalCoordinates(
+						latLng1.lat, latLng1.lng);
 
+				// expect(point1bis.lat).toBe(point1.lat);
+				expect(point1bis.lat).toEqual(point1.lat);
+				// expect(point1bis.lng).toBe(point1.lng);
+				expect(point1bis.lng).toEqual(point1.lng);
+			});
 
 });
