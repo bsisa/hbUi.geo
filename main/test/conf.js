0c6bf82f7f5f3be33ba6114ed2082a6f0b8dcc71
refond <refon@pobox.com>
2016-05-20 11:21:00 +0200

    Provides mocked config info. Here clientDebugEnable var is neccessary to hbUi.geo module intialisation.

---
+++ main/test/conf.js
@@ -0,0 +1,8 @@
+
+// ================================================================
+// ==  Mocks config info obtained at:                            ==
+// ==  http://rest.server.location/config.js                     ==
+// ================================================================
+
+//var apiBaseUrl = "http://localhost:9000/api/melfin/" ;
+var clientDebugEnabled = true;
