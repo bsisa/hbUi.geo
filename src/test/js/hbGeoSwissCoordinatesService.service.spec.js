6cf4da32bcc17df7b2a2499315a65527afc601e9
refond <refon@pobox.com>
2016-07-06 13:03:56 +0200

    Clean up but totally useless tests failing silently. Must be changed to end to end test, this type of `unit` testing is both complex and not related to actual runtime context which we want to test.

--- src/test/js/hbGeoSwissCoordinatesService.service.spec.js
+++ src/test/js/hbGeoSwissCoordinatesService.service.spec.js
@@ -1,6 +1,17 @@
 
 /**
+ * WARNING: These tests ARE NOT EFFECTIVE ! 
+ * 
+ * The hbGeoSwissCoordinatesService.getLongitudeLatitudeCoordinates fails silently, expect are never 
+ * reached... Further investigations shows going further in that direction requires so much plumbing
+ * related to the test context that is misses completely the final goal of testing the library as 
+ * close as possible to production context. 
+ * 
+ * Test context requires its own expertise irrelevant to the runtime execution context and brings in
+ * its own problems which do not exists in runtime execution context.  
+ * 
  * TODO: WE WANT END TO END TEST HERE, NEED TO MOVE TO SOMETHING LIKE http://www.protractortest.org/
+ * 
  * 
  * http://www.swisstopo.admin.ch/internet/swisstopo/en/home/products/software/products/skripts.html
  * 
@@ -17,6 +28,7 @@ describe('Swiss Federal (plane) to Latitude, Longitude (spheric) coordinates con
 
 	beforeEach(function() {
 		module('hbUi.geo')
+		//angular.mock.module('hbUi.geo')
 	});
 
 	
@@ -28,7 +40,7 @@ describe('Swiss Federal (plane) to Latitude, Longitude (spheric) coordinates con
 	// Wrap the parameter in underscores
 	beforeEach(inject(function(_hbGeoSwissCoordinatesService_, _$log_) {
 		hbGeoSwissCoordinatesService = _hbGeoSwissCoordinatesService_;
-		$log = _$log_;		
+		$log = _$log_;
 	}));
 	
 	// Log debug messages in Karma
@@ -50,31 +62,33 @@ describe('Swiss Federal (plane) to Latitude, Longitude (spheric) coordinates con
 				"z" : 477.000     // => 526.513       back =>     477.000
 			};
 		
-		$log.debug("REMOTE: point1 to convert : " + point1.x + ", " + point1.y);
+		$log.debug("REMOTE 1): point1 to convert : " + point1.x + ", " + point1.y);
 		
-		//hbGeoSwissCoordinatesService.getLongitudeLatitudeCoordinates().get({"easting" : point1.x, "northing" : point1.y, "format" : "json"}).then(
+		// Get GPS for swiss coordinates
 		hbGeoSwissCoordinatesService.getLongitudeLatitudeCoordinates(point1.x, point1.y).get().then(
 				function(latLng1) {
-
-					//$log.debug("REMOTE: latLng result                            : " + latLng1.easting + ", " + easting.northing);
-					
-//					var point1bis = hbGeoSwissCoordinatesService.getSwissFederalCoordinates(latLng1.lat, latLng1.lng);
-
 					$log.debug("2) REMOTE: point1 swiss: x, y                       : " + point1.x + ", " + point1.y);
-					$log.debug("2) REMOTE: latLng1 swiss => latlng approx: lat, lng : " + latLng1.lat + ", " + latLng1.lng);
-//					$log.debug("REMOTE: point1bis latlng => swiss approx: x,   y : " + point1bis.x + ", " + point1bis.y);
-					
-//					expect(latLng1.)
-//					expect(point1bis.x).toEqual(point1.x);
-//					expect(point1bis.y).toEqual(point1.y);						
-					
+					$log.debug("2) REMOTE: latLng1.xEastingLng = " + latLng1.xEastingLng + ", latLng1.yNorthingLat = " + latLng1.yNorthingLat);
+					// Get swiss coordinates back from GPS in order to check for deviation.
+					hbGeoSwissCoordinatesService.getSwissFederalCoordinates(latLng1.xEastingLng,latLng1.yNorthingLat).get().then(
+							function(point1bis) {
+            					$log.debug("REMOTE 2.2): point1bis.xEastingLng = " + point1bis.xEastingLng + ", point1bis.yNorthingLat = " + point1bis.yNorthingLat);
+            	            	$log.debug("REMOTE 2.2): point1bis" + angular.toJson(point1bis));
+            	            	expect(point1bis.xEastingLng).toEqual(point1.x);
+            	            	expect(point1bis.yNorthingLat).toEqual(point1.y);
+							}, 
+            				function(response) {
+            					$log.debug("REMOTE 2.2): FAILURE WITH response = " + angular.toJson(response));
+            				}
+            			);					
 				}, 
 				function(response) {
-					$log.debug("REMOTE: FAILURE WITH response = " + angular.toJson(response));
+					//$log.debug("REMOTE: FAILURE WITH response = " + angular.toJson(response));
+					$log.debug("REMOTE 2): FAILURE WITH response = " + response);
 				}
 			);
 		
-		$log.debug("REMOTE: end...");
+		$log.debug("REMOTE: 3) end...");
 	});
 
 });
